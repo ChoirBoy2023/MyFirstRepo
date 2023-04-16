09b064e56d1b356dd20133ad4abe4bf5cd3f122a
ChoirBoy2023 <126348565+ChoirBoy2023@users.noreply.github.com>
2023-04-15 20:25:26 -0400

    5th commit

--- main.c
+++ main.c
@@ -23,24 +23,33 @@ int main( int argc, char** argv )
          for( int32_t i = 0; i < numOfNumbers; ++i )
             fprintf(stdout,"numbers[%2.2d] = %d\n",i,numbers[i]);
 
-         free( numbers );
-         numbers = NULL;
-      }
+         numOfCars = 8;
+         if( (cars = (int32_t*) calloc( numOfCars, sizeof(int32_t) )) != NULL )
+         {
+            for( int32_t i = 0; i < numOfCars; ++i )
+               cars[i] = numbers[i] * 4;
 
-      numOfCars = 8;
-      if( (cars = (int32_t*) calloc( numOfCars, sizeof(int32_t) )) != NULL )
+            for( int32_t i = 0; i < numOfCars; ++i )
+               fprintf(stdout,"cars[%2.2d] = %d\n",i,cars[i]);
+
+            free( cars );
+            cars = NULL;
+
+            free( numbers );
+            numbers = NULL;
+         }
+         else  // Error case - cars
+         {
+            perror("Unable to allocate the cars!");
+         }
+      }
+      else  // Error case - numbers
       {
-         for( int32_t i = 0; i < numOfCars; ++i )
-            cars[i] = numbers[i] * 4;
-
-         for( int32_t i = 0; i < numOfCars; ++i )
-            fprintf(stdout,"cars[%2.2d] = %d\n",i,cars[i]);
-
-         free( cars );
-         cars = NULL;
+         perror("Unable to allocate the numbers!");
       }
+
    }
-   else  // Error case
+   else  // Error case - string
    {
       perror("Unable to allocate a string");
    }
