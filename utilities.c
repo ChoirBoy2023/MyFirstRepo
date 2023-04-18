604f0e8083eaca365646ed360b746d388c624208
ChoirBoy2023 <126348565+ChoirBoy2023@users.noreply.github.com>
2023-04-17 21:29:47 -0400

    first commit

---
+++ utilities.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void printBlue( void );
+void printRed( void );
+void printGreen( void );
+
+void printBlue( void )
+{
+   printf("I'm blue!\n");
+   return;
+}
+
+void printRed( void )
+{
+   printf("I'm red!\n");
+   return;
+}
+
+void printGreen( void )
+{
+   printf("I'm green!\n");
+   return;
+}
